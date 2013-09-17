c66b2c638d9f3bba07a0dd3a50756c6b8cc8ec70
shycherry.vaio <vinpowful@free.fr>
2013-09-17 22:16:47 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# itemTagsWatcher
+
+Watch files / directories systems such as FTP, LocalFS etc... and convert them into items tagged according to user's rules
---
+++ watcher.js
@@ -0,0 +1,39 @@
+﻿module.exports = function (options) {
+   
+  /**
+   * Module options
+   */  
+  var itDB = null
+  if ('undefined' != typeof options) _set_options_(options)
+  
+  /**
+   * Privates
+   */
+  function _set_options_(options){
+    if('undefined' != typeof options.database){
+      itDB = require('itemTagsDB'){database: options.database}
+      itDB.dumpDB()
+    }  
+  }
+  
+  
+  /**
+   * Protected
+   */
+  function deleteOne (uuid, callback) {
+    nosql.remove(_uuidFilter_(uuid), function(removedCount){
+      callback(undefined, removedCount)
+    })
+  }
+  
+
+  /**
+  * exposed API
+  */
+  return {
+    
+    "configure": _set_options_,    
+
+  }
+ 
+}
