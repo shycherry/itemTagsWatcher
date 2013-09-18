380dcf0daad917fd9fce0a3572d845673e2c2a7f
shycherry.vaio <vinpowful@free.fr>
2013-09-19 01:13:09 +0200

    wip

--- watcher.js
+++ watcher.js
@@ -1,62 +1,73 @@
-﻿module.exports = function (options) {
-   
-  /**
-   * Module options
-   */  
-  var itDB = null
-  var configDB = null
-  if ('undefined' != typeof options) _set_options_(options)
-  
-  /**
-   * Privates
-   */
-  function _set_options_(options){
-    if('undefined' != typeof options.database){
-      itDB = require('itemTagsDB')({database: options.database})      
-    }
-    if('undefined' != typeof options.configDB){
-      configDB = require('itemTagsDB')({database: options.configDB})      
-    }
-  }
-  
-  
-  /**
-   * Protected
-   */
-  function deleteOne (uuid, callback) {
-    nosql.remove(_uuidFilter_(uuid), function(removedCount){
-      callback(undefined, removedCount)
-    })
-  }
+﻿EventEmitter = require('events').EventEmitter;
+inherits = require('util').inherits;
 
-  function listFtp(){
-    if(!configDB){return}
-    configDB.fetchItemsSharingTags(['@ftpConfig'], function(err, items){
+var Watcher = module.exports = function (options) {
+
+  if(!(this instanceof Watcher))
+    return new Watcher(options);
+
+  var _itDB;
+  var _configDB;
+  
+  if ('undefined' != typeof options) this.configure(options);
+};
+
+inherits(Watcher, EventEmitter);
+
+/**
+* exposed API
+*/
+Watcher.prototype.configure = configure;
+Watcher.prototype.listFtp = listFtp;
+Watcher.prototype.doFtpWatch = doFtpWatch;
+Watcher.prototype.getDB = function(){return this._itDB;};
+
+
+/**
+* methods
+*/
+function configure(options){
+  var self = this;
+  if('undefined' != typeof options.configDB){
+    this._configDB = require('itemTagsDB')({database: options.configDB});
+    this._configDB.fetchItemsSharingTags(['@watcherConfig'], function(err, items){
       if(!err){
-        var ftpConfig = items[0]
-        var ftp = require('ftp')()
-        ftp.on('ready', function(){
-          ftp.list(function(err, list){
-            console.dir(list)
-            ftp.end()
-          })
-        })
-        ftp.connect(ftpConfig)
-      }      
-    })
-  }  
-
-  /**
-  * exposed API
-  */
-  return {
-    
-    "configure": _set_options_,
-
-    "listFtp": listFtp,
-
-    "getDB": function getDB(){return itDB}
-
+        var watcherConfig = items[0];
+        if(watcherConfig.database){
+          self._itDB = require('itemTagsDB')({database: watcherConfig.database});
+          self.emit('ready');
+        }
+      }
+    });
   }
- 
+}
+
+function doFtpWatch(){
+  var self = this;
+  if(!this._configDB){return;}
+  this._configDB.fetchItemsSharingTags(['@ftpWatchingSet'], function(err, items){
+    if(!err){
+      for(var iSet = 0; iSet<items.length; iSet++){
+        var ftpWatchingSet = items[iSet];
+        self._configDB.fetchOne(ftpWatchingSet.ftpConfig, (function(ftpWatchingSet){
+          return function(err, itemFtpConfig){
+            if(!err){
+              self.listFtp(itemFtpConfig.config, ftpWatchingSet.path);
+            }
+          };
+        })(ftpWatchingSet));
+      }
+    }
+  });
+}
+
+function listFtp(ftpConfig, path){
+  var ftp = require('ftp')();
+  ftp.on('ready', function(){
+    ftp.list(path, function(err, list){
+      console.dir(list);
+      ftp.end();
+    });
+  });
+  ftp.connect(ftpConfig);
 }
