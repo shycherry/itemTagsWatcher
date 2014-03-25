dfd5e5430f545cb86e42d23b65971f07189fe0cd
shycherry.vaio <vinpowful@free.fr>
2014-03-25 19:18:27 +0100

    wip tests

--- watcher.js
+++ watcher.js
@@ -218,3 +218,32 @@ function _handleDummyPaths_(iDummyWatchPath, iCallback){
   iCallback(undefined, watchReport);
 
 }
+
+function copyFile(source, target, cb) {
+  //create a copy of db
+  if(fs.existsSync(target)){
+    fs.unlinkSync(target);
+  }
+
+  var cbCalled = false;
+
+  var rd = fs.createReadStream(source);
+  rd.on("error", function(err) {
+    done(err);
+  });
+  var wr = fs.createWriteStream(target);
+  wr.on("error", function(err) {
+    done(err);
+  });
+  wr.on("close", function(ex) {
+    done();
+  });
+  rd.pipe(wr);
+
+  function done(err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
+    }
+  }
+}
